f871ee824aee805953b3f5e594f87341e442ebc6
rachel4w2 <87238236+rachel4w2@users.noreply.github.com>
2023-06-05 21:12:27 +0900

    Blog_Introduction

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,17 +1,20 @@
-const Resume = () => {
-  return;
-  <div>
-    <h3>기초프로젝트랩 수강</h3>
-    <ul>
-      <li>2022.03 ~ 2022.06</li>
-      <li>CNU 버스앱 프로젝트</li>
-    </ul>
-    <h3>딥러닝 실무 프로젝트</h3>
-    <ul>
-      <li>2023.03 ~ 2022.06</li>
-      <li>논문요약 모델 생성 프로젝트</li>
-    </ul>
-  </div>;
+import React from 'react';
+
+const Resume: React.FC = () => {
+  return (
+    <div>
+      <h3>기초프로젝트랩 수강</h3>
+      <ul>
+        <li>2022.03 ~ 2022.06</li>
+        <li>CNU 버스앱 프로젝트</li>
+      </ul>
+      <h3>딥러닝 실무 프로젝트</h3>
+      <ul>
+        <li>2023.03 ~ 2022.06</li>
+        <li>논문요약 모델 생성 프로젝트</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Resume;
